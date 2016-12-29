6c77ebb7ba70b9635295c1dce323156a4a050239
fery ferlangga <pengusahaads@gmail.com>
2016-12-29 15:50:23 +0700

    Update utama.js

--- utama.js
+++ utama.js
@@ -24,7 +24,7 @@ var xxx=null;
 if(x=="1"){xxx="belajar-safelink.html"}
 if(x=="2"){xxx="blogger-safelink-premium.html"}
 if(x=="3"){xxx="random-post-safelink.html"}
-if(x=="4"){xxx="http://ampsafelink.blogspot.com/2016/12/safelink-premium-post.html"}a_to_ve[i].href="http://ampsafelink.blogspot.com/2016/12/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="4"){xxx="safelink-premium-post.html"}a_to_ve[i].href="https://ampsafelink.blogspot.com/2016/12/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
